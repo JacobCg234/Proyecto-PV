8cdfea7eead18b3513a754c812e36fbbf2607e38
Jacobo <jacocg10@gmail.com>
2025-10-18 10:42:41 -0600

    Piano

---
+++ src/main/java/org/example/orquesta/Instrumento.java
@@ -0,0 +1,5 @@
+package org.example.orquesta;
+
+public interface Instrumento {
+    void tocar();
+}
---
+++ src/main/java/org/example/orquesta/Piano.java
@@ -0,0 +1,8 @@
+package org.example.orquesta;
+
+public class Piano implements Instrumento{
+    @Override
+    public void tocar(){
+        System.out.println("Tocando Piano");
+    }
+}
